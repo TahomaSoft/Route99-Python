9fb43e13290f9c038e9e0cce65263c2f21241f1e
TahomaSoft <traderbeckola@tahoma.com>
2021-07-18 18:41:34 -0400

    init

---
+++ Route53dkim.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import textwrap
+import fileinput
+import sys
+
+
+LineIn= sys.stdin.readline()
+LineOut= textwrap.wrap(LineIn, width=70)          
+
+
+for k in LineOut:
+    k=k + '"' + ' ' + '"'
+    sys.stdout.write(k)
+    #    print (k)
+
+
+    
+
+
+
+
